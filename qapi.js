52f8ae95e8d6d90b26b3b69c897409adbd2184bc
Karelia18 <84497763+Karelia18@users.noreply.github.com>
2021-05-20 10:32:27 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60a608df2061a7001e1779bd",
+				"60a608df2061a7001e1779bf"
+			]
+		},
+		"60a608df2061a7001e1779bd": {
+			"id": "60a608df2061a7001e1779bd",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60a608df2061a7001e1779bf": {
+			"id": "60a608df2061a7001e1779bf",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
